3884034a79ff352d18374ac37c3fbc3227f3adaf
EliasPereirah <eliaspereirah@localhost>
2024-12-11 18:28:39 -0300

    Adding Gemini 2.0 Flash

--- js/script.js
+++ js/script.js
@@ -39,10 +39,9 @@ let PLATFORM_DATA = {
     },
     google: {
         models: [
+            "gemini-2.0-flash-exp",
             "gemini-exp-1206",
-            "gemini-exp-1121",
             "learnlm-1.5-pro-experimental",
-            "gemini-exp-1114",
             "gemini-1.5-pro",
             "gemini-1.5-flash",
             "gemini-1.5-flash-8b"
