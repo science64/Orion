92b404fa7b46cad24d4cdb2c65559dd72c73fab2
EliasPereirah <eliaspereirah@localhost>
2025-02-16 15:38:23 -0300

    Adding gemini-1.5-pro again

--- js/script.js
+++ js/script.js
@@ -44,6 +44,7 @@ let PLATFORM_DATA = {
     },
     google: {
         models: [
+            "gemini-1.5-pro",
             "gemini-2.0-pro-exp-02-05",
             "gemini-2.0-flash",
             "gemini-2.0-flash-thinking-exp-01-21",
